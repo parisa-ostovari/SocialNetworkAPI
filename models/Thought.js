f8bb6ed2d1b0c127d460b2371f3419c6128f2d13
Parisa Ostovari <parisaostovari@gmail.com>
2022-02-11 11:58:10 -0800

    thought.js completed. reaction.js began

--- models/Thought.js
+++ models/Thought.js
@@ -1,3 +1,40 @@
 const { Schema, model } = require('mongoose');
 const reactionSchema = require('./Reaction');
-const dateFormat = require('../utils/dateFormater');
+const dateFormater = require('../utils/dateFormater');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required : 'Please leave a thought.',
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timeStamp => dateFormater(timeStamp)
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema]
+    },
+
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
